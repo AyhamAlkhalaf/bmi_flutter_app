e9437ae69ef694c72c63aff2c0313e924f1e016a
AyhamAlkhalaf <ayham.ayham909@gmail.com>
2021-07-20 13:15:52 -0700

    rest

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -22,7 +22,7 @@ class LoginScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Login aa',
+                    'Login',
                     style: TextStyle(
                       fontSize: 40.0,
                       fontWeight: FontWeight.bold,
